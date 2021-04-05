c399ae5a312be6ddf35a1aea77acee292c92f42d
Shreya Koley <68245668+shreyakoley020102@users.noreply.github.com>
2021-04-05 22:09:48 +0530

    Add files via upload

--- src/js/controller.js
+++ src/js/controller.js
@@ -35,6 +35,8 @@ const showRecipe = async function () {
     const id = window.location.hash.slice(1);
     console.log(id);
 
+    if (!id) return;
+
     // 1)Loading recipes
     renderSpinner(recipeContainer);
 
