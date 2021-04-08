cdbfecb22aa59ecd57e82ba3338d98f60f77c102
Shreya Koley <68245668+shreyakoley020102@users.noreply.github.com>
2021-04-08 13:55:06 +0530

    Add files via upload

--- src/js/model.js
+++ src/js/model.js
@@ -5,25 +5,29 @@ export const state = {
 };
 
 export const loadRecipe = async function (id) {
-  const res = await fetch(
-    //'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcd09'
-    `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
-  );
+  try {
+    const res = await fetch(
+      //'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcd09'
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
+    );
 
-  const data = await res.json();
+    const data = await res.json();
 
-  if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
 
-  const { recipe } = data.data;
-  state.recipe = {
-    id: recipe.id,
-    title: recipe.title,
-    publisher: recipe.publisher,
-    sourceUrl: recipe.source_url,
-    image: recipe.image_url,
-    servings: recipe.servings,
-    cookingTime: recipe.cooking_time,
-    ingredients: recipe.ingredients,
-  };
-  console.log(state.recipe);
+    const { recipe } = data.data;
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(state.recipe);
+  } catch (err) {
+    alert(err);
+  }
 };
