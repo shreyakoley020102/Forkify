4b07c1feec78b8a69754ef8a2da3ba587efbb857
Shreya Koley <68245668+shreyakoley020102@users.noreply.github.com>
2021-04-08 13:39:01 +0530

    Add files via upload

--- src/js/model.js
+++ src/js/model.js
@@ -4,4 +4,26 @@ export const state = {
   recipe: {},
 };
 
-export const loadRecipe = async function () {};
+export const loadRecipe = async function (id) {
+  const res = await fetch(
+    //'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcd09'
+    `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
+  );
+
+  const data = await res.json();
+
+  if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+  let { recipe } = data.data;
+  recipe = {
+    id: recipe.id,
+    title: recipe.title,
+    publisher: recipe.publisher,
+    sourceUrl: recipe.source_url,
+    image: recipe.image_url,
+    servings: recipe.servings,
+    cookingTime: recipe.cooking_time,
+    ingredients: recipe.ingredients,
+  };
+  console.log(recipe);
+};
